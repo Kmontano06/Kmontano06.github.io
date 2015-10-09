78f371ea2e9d883eeecaca549cab405974dd1f12
Kmontano06 <keniamontano06@gmail.com>
2015-10-08 22:34:55 -0600

    Cambios en los Proyectos, Header y Contacto

--- js/script.js
+++ js/script.js
@@ -14,53 +14,16 @@ app.proyectos = function(){
   return{
     imprimirProyectos:function(proyectos){
       var list_proyectos ="";
-      for(i=0; i < proyectos.length; i++){
-         list_proyectos += '<li class="list-project-item"><div class="container-left"><h2 class="container-left-title">'+proyectos[i].nombre+'</h2><img class="container-left-image" src="'+proyectos[i].imagen+'"/></div><div class="container-right"><p class="container-right-description">'+proyectos[i].descripcion+'</p><a class="container-right-link" href="'+proyectos[i].url+'">Ir al Sitio</a></div></li>'
+      for(var i=0; i < proyectos.length; i++){
+         list_proyectos += '<li class="list-project-item"><img class="list-project-image" src="'+proyectos[i].imagen+'"/><p class="list-project-description">'+proyectos[i].descripcion+'</p><a class="list-project-link" target="_blank" href="'+proyectos[i].url+'">Ir al Sitio</a></li>'
       }
       document.getElementById("proyectos").innerHTML= "<ul class='list-project'>"+list_proyectos+"</ul>";
+      app.proyectos.colorProjects(proyectos);
+    },
+    colorProjects:function(proyectos){
+      for(var j=0; j<proyectos.length;j++){
+        document.getElementsByClassName("list-project-item")[j].style.background=proyectos[j].color;
+      }
     }
   }; 
 }();
-function validacionForm() {
- var email_correcto=false;
- var name_correcto=false;
- var textarea_correcto=false;
- var name = document.getElementById("name-value").value;
- if(name.length === 0 || name.length <= 10){
-  document.getElementById("message-name").className="content-title-input incorrecto";
-   document.getElementById("message-name").innerHTML="Nombre: Nombre Incorrecto! Ingreselo Nuevamente";
-
- }
- else{
-  document.getElementById("message-name").className="content-title-input correcto";
-  document.getElementById("message-name").innerHTML="Nombre:";
-  name_correcto=true;
- }
-var email = document.getElementById("email").value;
-    var email_patron = new RegExp(/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/);
-    var resultado_email = email_patron.test(email);
-    if(resultado_email === true){
-      document.getElementById("message-email").className="content-title-input correcto";
-      document.getElementById("message-email").innerHTML="Correo:";
-      email_correcto=true;
-    }else{
-      document.getElementById("message-email").className="content-title-input incorrecto";
-        document.getElementById("message-email").innerHTML="Correo: Correo Incorrecto! Ingreselo Nuevamente";
-    }
-    var textarea = document.getElementById("textarea-value").value;
-    if(textarea.length === 0 || textarea.length <= 10){
-      document.getElementById("message-textarea").className="content-title-input incorrecto";
-      document.getElementById("message-textarea").innerHTML="Mensaje: Mensaje Incorrecto! Ingreselo Nuevamente";
-
-    }else{
-      document.getElementById("message-textarea").className="content-title-input correcto";
-      document.getElementById("message-textarea").innerHTML="Mensaje:";
-      textarea_correcto=true;
-    }
-
-    if(name_correcto === true && email_correcto === true && textarea_correcto === true){
-      alert("El mensaje ha sido enviado");
-      // Este codigo hace que la pagina se refresque
-      window.location.reload();  
-    }
-};
